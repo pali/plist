5beb2ca684e5d9747419dd5f15eb1f7a3b51cc05
Pali Rohár <pali.rohar@gmail.com>
2014-02-16 12:27:30 +0100

    PList:List::Binary: Change binary list format, add 512 kB header with indexed offsets to emails

--- PList/List/Binary.pm
+++ PList/List/Binary.pm
@@ -16,27 +16,98 @@ sub lengthbytes($) {
 
 }
 
-sub new($$) {
+sub new($$$) {
 
 	my ($class, $filename, $readonly) = @_;
 
 	my $mode;
 	my $fh;
+	my @offsets = ();
 
 	if ( $readonly ) {
 		$mode = "<:mmap:raw";
 	} else {
-		$mode = ">>:raw";
+		$mode = "+<:raw";
 	}
 
-	if ( not open($fh, $mode, $filename) ) {
-		return undef;
+	my $ret = open($fh, $mode, $filename);
+
+	if ( not $ret ) {
+
+		if ( $readonly ) {
+			warn "Cannot open file $filename: $!\n";
+			return undef;
+		} else {
+			$ret = open($fh, "+>:raw", $filename);
+			if ( not $ret ) {
+				warn "Cannot create file $filename: $!\n";
+				return undef;
+			}
+			my $header = "list";
+			$header .= pack("V", 0) for(1..131071);
+			print $fh $header;
+		}
+
+	} else {
+
+		if ( not seek($fh, 0, 2) ) {
+			warn "File $filename is not seekable: $!\n";
+			close($fh);
+			return undef;
+		}
+
+		my $size = tell($fh);
+		if ( not $size ) {
+			warn "File $filename is not seekable: $!\n";
+			close($fh);
+			return undef;
+		}
+
+		seek($fh, 0, 0);
+
+		my $sig;
+		read($fh, $sig, 4);
+		if ( $sig ne "list" ) {
+			warn "File $filename does not have header signature\n";
+			close($fh);
+			return undef;
+		}
+
+		my $was_last = 0;
+		for (1..131071) {
+			my $off;
+			if ( not read($fh, $off, 4) ) {
+				warn "File $filename has small header\n";
+				close($fh);
+				return undef;
+			}
+			$off = unpack("V", $off);
+			if ( $off == 0 ) {
+				$was_last = 1;
+			} elsif ( $off > $size ) {
+				warn "File $filename has corrupted header\n";
+				close($fh);
+				return undef;
+			}
+			if ( not $was_last ) {
+				push(@offsets, $off);
+			}
+		}
+
+		my $off = tell($fh);
+		if ( $off != 524288 ) {
+			warn "File $filename is corrupted\n";
+			close($fh);
+			return undef;
+		}
+
 	}
 
 	my $priv = {
 		fh => $fh,
 		readonly => $readonly,
-		eof => 0,
+		offsets => \@offsets,
+		next => 0,
 	};
 
 	return bless $priv, $class;
@@ -54,100 +125,124 @@ sub append($$) {
 
 	my ($priv, $pemail) = @_;
 
-	return 0 if $priv->{readonly};
+	if ( $priv->{readonly} ) {
+		warn "File is readonly\n";
+		return undef;
+	}
 
 	my $fh = $priv->{fh};
+	my $offsets = $priv->{offsets};
 
 	my $str = PList::Email::Binary::to_str($pemail);
-	return 0 unless $str;
+	if ( not $str ) {
+		warn "Cannot read email\n";
+		return undef;
+	}
+
+	my $num = scalar @{$offsets};
+	my $offset;
+
+	if ( $num != 0 ) {
+
+		$offset = ${$offsets}[$num-1];
+
+		if ( not seek($fh, $offset, 0) ) {
+			warn "File is corrupted\n";
+			return undef;
+		}
+
+		my $len;
+		if ( not read($fh, $len, 4) ) {
+			warn "File is corrupted\n";
+			return undef;
+		}
+
+		$offset += 4 + unpack("V", $len);
+
+	} else {
+
+		$offset = 524288;
+
+	}
+
+	seek($fh, 0, 2);
+	if ( tell($fh) != $offset ) {
+		warn "File is corrupted\n";
+		return undef;
+	}
 
 	print $fh pack("V", lengthbytes($str));
 	print $fh $str;
 
-	return 1;
+	push(@{$offsets}, $offset);
+	seek($fh, 4*(1+$num), 0);
+	print $fh pack("V", $offset);
+
+	return $num;
+
+}
+
+sub count($) {
+
+	my ($priv) = @_;
+	return scalar @{$priv->{offsets}};
 
 }
 
 sub eof($) {
 
 	my ($priv) = @_;
-	return $priv->{eof} if $priv->{eof};
-	return eof($priv->{fh});
+	return ( $priv->{next} >= $priv->count() );
 
 }
 
 sub reset($) {
 
 	my ($priv) = @_;
-
-	return 0 unless $priv->{readonly};
-
-	seek($priv->{fh}, 0, 0);
-	$priv->{eof} = 0;
+	$priv->{next} = 0;
 
 }
 
-sub offset($) {
+sub readnum($$) {
 
-	my ($priv) = @_;
-	return tell($priv->{fh});
-
-}
-
-sub skipnext($) {
-
-	my ($priv) = @_;
+	my ($priv, $num) = @_;
 
 	my $fh = $priv->{fh};
+	my $offsets = $priv->{offsets};
 
-	if ( $priv->{eof} or not $priv->{readonly} ) {
-		return 0;
-	}
-
-	my $len;
-
-	if ( not read($fh, $len, 4) ) {
-		$priv->{eof} = 1;
-		return 0;
-	}
-
-	$len = unpack("V", $len);
-
-	if ( not seek($fh, $len, 1) ) {
-		$priv->{eof} = 1;
-		return 0;
-	}
-
-	return 1;
-
-}
-
-sub readnext($) {
-
-	my ($priv) = @_;
-
-	my $fh = $priv->{fh};
-
-	if ( $priv->{eof} or not $priv->{readonly} ) {
+	if ( $num >= $priv->count() ) {
 		return undef;
 	}
 
 	my $len;
 	my $str;
 
+	if ( not seek($fh, ${$offsets}[$num], 0) ) {
+		warn "File corrupted\n";
+		return undef;
+	}
+
 	if ( not read($fh, $len, 4) ) {
-		$priv->{eof} = 1;
+		warn "File corrupted\n";
 		return undef;
 	}
 
 	$len = unpack("V", $len);
 
 	if ( not read($fh, $str, $len) ) {
-		$priv->{eof} = 1;
+		warn "File corrupted\n";
 		return undef;
 	}
 
 	return PList::Email::Binary::from_str($str);
+
+}
+
+sub readnext($) {
+
+	my ($priv) = @_;
+	return undef if $priv->eof();
+	return $priv->readnum($priv->{next}++);
 
 }
 
