bc690a483f25d4383dc7b085661a15e42d9a0983
Pali Rohár <pali.rohar@gmail.com>
2014-02-18 15:34:02 +0100

    PList::Index: Update info

--- PList/Index.pm
+++ PList/Index.pm
@@ -3,11 +3,34 @@ package PList::Index;
 use strict;
 use warnings;
 
-sub new($$) {
+use File::Copy;
 
-	my ($class, $dir) = @_;
+# OLD!!!
+# structure:
+# imported_[0-9]{5}.mbox
+# [0-9]{5}.mbox
+# imported_[0-9]{5}.list
+# [0-9]{5}.list
+# [0-9]{5}.offs
+# threads
+# mark_deleted.txt
 
-	my $priv = {};
+# NEW:
+# [0-9]{5}.list
+# threads
+# deleted
+
+sub new($$;$) {
+
+	my ($class, $dir, $create) = @_;
+
+	if ( $create ) {
+		die "Cannot create directory $dir" unless mkpath($dir);
+	}
+
+	my $priv = {
+		dir => $dir,
+	};
 
 	bless $priv, $class;
 
@@ -19,21 +42,37 @@ sub DESTROY($) {
 
 # file functions
 
-sub add_mbox($$) {
+# priv, mbox
+#sub add_mbox($$) {
+#
+#	my ($priv, $mbox) = @_;
+#
+#	my $dh;
+#	return undef unless opendir($dh, $priv->{dir});
+#	my @imported = sort grep { /^imported_[0-9]{5}.mbox$/ } readdir $dh;
+#	my $last = $imported[-1];
+#	$last =~ s/^imported_([0-9]{5}).mbox$/$1/;
+#	$last = sprintf("%05d", $last+1);
+#
+#	copy($mbox, $priv->{dir} . "/imported_" . $last . ".mbox");
+#
+#}
 
-}
-
+# priv, pemail
 sub add_email($$) {
 
+	my ($priv, $pemail) = @_;
+
 }
 
+# priv, $id, $up, $file, $offset
 sub add_threads($$) {
 
 }
 
 # recovery functions
 
-sub regenerate_lists($) {
+sub regenerate_lists_headers($) {
 
 }
 
