7d4a728841121c0411cca6dc9d6aedaea396d18b
Pali Rohár <pali.rohar@gmail.com>
2014-03-02 22:59:10 +0100

    PList::Email::View: Add DOCTYPE to default base template

--- PList/Email/View.pm
+++ PList/Email/View.pm
@@ -26,6 +26,9 @@ my $t2h = HTML::FromText->new();
 my @disabled_mime_types_default = qw(application/pgp-signature);
 
 my $base_template_default = <<END;
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	 "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
