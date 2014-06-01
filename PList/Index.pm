99cdf32dfa1c53c7ab214b9a2afae24bfeda76c2
Pali Rohár <pali.rohar@gmail.com>
2014-06-01 23:18:19 +0200

    PList::Index: db_graph: desc param is not used anymore, so remove it - it speed up sql query because ORDER BY is not used anymore

--- PList/Index.pm
+++ PList/Index.pm
@@ -1055,28 +1055,20 @@ sub db_treeid($$$) {
 
 }
 
-sub db_graph($$;$) {
+sub db_graph($$) {
 
-	my ($priv, $treeid, $desc) = @_;
+	my ($priv, $treeid) = @_;
 
 	my $dbh = $priv->{dbh};
 
 	my $statement;
 	my $ret;
 
-	if ( $desc ) {
-		$desc = "DESC";
-	} else {
-		$desc = "";
-	}
-
 	$statement = qq(
-		SELECT e1.id AS id1, e2.id AS id2, r.type AS type
+		SELECT r.emailid2 AS id1, r.emailid1 AS id2, r.type AS type
 			FROM emails AS e1
 			JOIN replies AS r ON r.emailid2 = e1.id
-			JOIN emails AS e2 ON e2.id = r.emailid1
 			WHERE e1.treeid = ?
-			ORDER BY r.type, e1.date, e2.date $desc
 		;
 	);
 
