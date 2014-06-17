5d568963bec1ba81654b8dee9f72e6e24caa6bc8
Pali Rohár <pali.rohar@gmail.com>
2014-06-17 13:52:57 +0200

    PList::List::MBox4: New module which using patched Email::Folder::Mbox

---
+++ PList/List/MBox4.pm
@@ -0,0 +1,67 @@
+package PList::List::MBox4;
+
+use strict;
+use warnings;
+
+use base "PList::List";
+
+use PList::Email::MIME;
+
+use lib '.';
+use Email::Folder::Mbox;
+
+sub new($$) {
+
+	my ($class, $arg) = @_;
+
+	my $is_fh = 0;
+	{
+		$@ = "";
+		my $fd = eval { fileno $arg };
+		$is_fh = !$@ && defined $fd;
+	}
+
+	my @args;
+
+	if ( $is_fh ) {
+		push(@args, "FH");
+		push(@args, fh => $arg);
+	} else {
+		push(@args, $arg);
+	}
+
+	# NOTE: When jwz_From_ is set to 1 message separator is string "^From "
+	push(@args, jwz_From_ => 1);
+
+	# NOTE: When unescape is set to 1 every "^>+From " line is unescaped
+	push(@args, unescape => 1);
+
+	my $mbox = Email::Folder::Mbox->new(@args);
+	return undef unless ref $mbox;
+
+	return bless \$mbox, $class;
+
+}
+
+sub eof($) {
+
+	my ($mbox) = @_;
+
+	return 0 unless ${$mbox}->{_fh};
+	return eof(${$mbox}->{_fh});
+
+}
+
+sub readnext($) {
+
+	my ($mbox) = @_;
+
+	my $from = ${$mbox}->next_from();
+	my $message = ${$mbox}->next_message();
+	my $messageid = ${$mbox}->messageid();
+
+	return PList::Email::MIME::from_str(\$message, $from, $messageid);
+
+}
+
+1;
