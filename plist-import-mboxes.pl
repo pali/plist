cb545b6fc0ef751eb21d3d444dffbb7f0f9d7eb3
Pali Rohár <pali.rohar@gmail.com>
2014-06-21 16:23:48 +0200

    plist-import-mboxes.pl: Script for importing multiple mboxes with timestamp caching

---
+++ plist-import-mboxes.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use PList::Index;
+
+use FindBin qw($Bin);
+
+sub help() {
+
+	print "help:\n";
+	print "<dir> <mbox1> [<mbox2> ...] [silent]\n";
+	exit 1;
+
+}
+
+my $indexdir = shift @ARGV;
+help() unless $indexdir;
+
+my $silent;
+if ( @ARGV and $ARGV[-1] eq "silent" ) {
+	$silent = "silent";
+	pop(@ARGV);
+}
+
+my @mboxes = @ARGV;
+help() unless @mboxes;
+
+my $index = new PList::Index($indexdir);
+die "Error: Cannot open index dir '$indexdir'\n" unless $index;
+
+$index = undef;
+
+my %timestamps;
+
+if ( open(my $file, "<", "$indexdir/timestamps") ) {
+	while (<$file>) {
+		chomp($_);
+		my ($time, $file) = split(" ", $_, 2);
+		next unless $time and $file;
+		$timestamps{$file} = $time;
+	}
+}
+
+my $count = 0;
+
+foreach my $mbox (@mboxes) {
+	if ( not -f $mbox ) {
+		warn "Error: File '$mbox' does not exist\n";
+		next;
+	}
+	my $time = (stat($mbox))[9];
+	if ( not exists $timestamps{$mbox} or $timestamps{$mbox} < $time ) {
+		my $ret = system("$Bin/plist.pl", "index", "add-mbox", $indexdir, $mbox, $silent);
+		if ( $ret == 0 ) {
+			$timestamps{$mbox} = $time;
+			$count++;
+		}
+	}
+}
+
+print "Processed $count mboxes\n";
+
+if ( $count ) {
+	my $file;
+	if ( open(my $file, ">", "$indexdir/timestamps") ) {
+		print $file $timestamps{$_} . " $_\n" foreach sort keys %timestamps;
+		close($file);
+	} else {
+		warn "Error: Cannot store mbox timestamps\n";
+	}
+}
