0310c913df2b34ad0223fbdee75499b86c6348b9
Pali Rohár <pali.rohar@gmail.com>
2014-07-18 16:45:11 +0200

    PList::Email::MIME: Module Email::MIME using MIME-Header decoder also for Message-Id, so use it also in from_str() to set same Message-Id

--- PList/Email/MIME.pm
+++ PList/Email/MIME.pm
@@ -683,6 +683,9 @@ sub from_str($;$$) {
 	}
 
 	if ( $messageid ) {
+		my $new_messageid;
+		eval { $new_messageid = decode("MIME-Header", $messageid); 1 };
+		$messageid = $new_messageid if $new_messageid;
 		my @ids = ids($messageid);
 		$id = $ids[0] if @ids;
 	}
