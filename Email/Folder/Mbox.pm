ac2d242adfb6a7303809d6ae4c6fce45d05df524
Pali Rohár <pali.rohar@gmail.com>
2015-01-31 23:06:36 +0100

    Email::Folder::Mbox: Do not cut last line of last email in mbox

--- Email/Folder/Mbox.pm
+++ Email/Folder/Mbox.pm
@@ -174,7 +174,6 @@ sub next_messageref {
         }
 
         if ($prev eq $/ && ($line =~ $self->_from_line_re)) {
-            $mail .= $prev;
             $last = $line;
             last;
         }
@@ -189,6 +188,7 @@ sub next_messageref {
         # unescape From_
         $prev =~ s/^>(>*From )/$1/ if $self->{unescape};
     }
+    $mail .= $prev;
     print "$count end of message line $.\n" if debug;
     $self->{from} = $last;
     return unless $mail;
