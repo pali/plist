39580ca8ddf8133322924f62c5896f109357f6ad
Pali Rohár <pali.rohar@gmail.com>
2015-06-08 10:03:57 +0200

    mm_cfg.py: Use add-email instead add-mbox

--- mm_cfg.py
+++ mm_cfg.py
@@ -1,2 +1,2 @@
-PUBLIC_EXTERNAL_ARCHIVER = '/usr/bin/plist.pl index add-mbox /var/lib/plist/%(listname)s'
-PRIVATE_EXTERNAL_ARCHIVER = '/usr/bin/plist.pl index add-mbox /var/lib/plist/%(listname)s'
+PUBLIC_EXTERNAL_ARCHIVER = '/usr/bin/plist.pl index add-email /var/lib/plist/%(listname)s'
+PRIVATE_EXTERNAL_ARCHIVER = '/usr/bin/plist.pl index add-email /var/lib/plist/%(listname)s'
